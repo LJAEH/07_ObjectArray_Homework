35b169b8a560d6e3a3e5a6ea11a77065ddaffe45
LJAEH <wogurdlek33@gmail.com>
2022-12-22 14:13:45 +0900

    day2

--- src/com/hw3/controller/ProductController.java
+++ src/com/hw3/controller/ProductController.java
@@ -1,5 +1,53 @@
 package com.hw3.controller;
 
+import java.util.Scanner;
+
+import com.hw3.model.vo.Product;
+
 public class ProductController {
 
+	private Product pro = null;
+	
+	public int count;
+	
+	Scanner sc = new Scanner(System.in);
+	
+	
+	public void mainMenu() {
+		int menuNum = 0;
+		
+		do {
+			System.out.println("= 제품 관리 메뉴 =");
+			System.out.println("1.제품 정보 추가");
+			System.out.println("2.제품 전체 조회");
+			System.out.println("0.프로그램 종료");
+			System.out.print("번호입력 >>>>");
+			menuNum = sc.nextInt();
+			
+			switch(menuNum){
+				
+				case 1 : System.out.println( productInput() );break;
+				case 2 : System.out.println( productPrint() );break;
+				case 0 : System.out.println( "프로그램을 종료합니다" );break;
+				default : System.out.println("메뉴번호 재입력");
+			}
+			
+		} while( menuNum != 0 );
+	}
+	
+	public String productInput() {
+		
+		
+		return "";
+	}
+	
+
+	public String productPrint() {
+		
+		
+		return "";
+	}
+	
+	
+	
 }
--- src/com/hw3/model/vo/Product.java
+++ src/com/hw3/model/vo/Product.java
@@ -1,5 +1,25 @@
 package com.hw3.model.vo;
 
 public class Product {
+	
+	private int pld; // 제품번호
+	private String pName; // 제품명
+	private int price; // 제품 가격
+	private double tax; // 제품 세금
+	
+	public Product() {}
+	
+	public Product(int pld, String pName, int price, double tax) {
+		this.pld = pld;
+		this.pName = pName;
+		this.price = price;
+		this.tax = tax;	
+	}
+	
+	public String infomation() {
+		return "제품번호 : " + pld + "제품명 : " + pName + "제품가격 : " 
+				+ price + "제품 세금 : " + tax;	
+	}
+	
 
 }
